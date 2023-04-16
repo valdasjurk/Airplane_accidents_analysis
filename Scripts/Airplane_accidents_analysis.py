09d319c4810bf1451dbcbb24037e063f775ba84b
valdasjurk <valdas.jurk@gmail.com>
2023-04-16 21:59:25 +0300

    changed functions names

--- Scripts/Airplane_accidents_analysis.py
+++ Scripts/Airplane_accidents_analysis.py
@@ -3,9 +3,7 @@
 import kaggle
 import pandas as pd
 
-# import numpy as np
-
-DIRECTORY = "data/raw/AviationData.csv"
+RAW_DATA_DIRECTORY = "data/raw/AviationData.csv"
 
 
 def download_dataset() -> None:
@@ -20,10 +18,9 @@ def download_dataset() -> None:
 
 def read_dataset():
     df = pd.read_csv(
-        DIRECTORY,
-        index_col=0,
+        RAW_DATA_DIRECTORY,
+        parse_dates=["Event.Date", "Publication.Date"],
         encoding="cp1252",
-        dtype={"name": "string", "Event_Date": "datetime64"},
     )
     return df
 
@@ -34,30 +31,18 @@ def column_name_replacement(df, what_to_replace: str, replacement: str) -> pd.Da
     return df
 
 
-def separate_city_and_state(df):
+def separate_city_and_state(df) -> pd.DataFrame:
     """Separates city and state"""
     df[["City", "State"]] = df["Location"].str.split(",", n=1, expand=True)
     return df
 
 
-def get_year_and_month_from_date(df):
+def get_year_and_month_from_date(df) -> pd.DataFrame:
     """Separates year and month from accident date"""
     df["Event_Date"] = pd.to_datetime(df["Event_Date"])
     df["Event_year"] = df["Event_Date"].dt.year
     df["Event_month"] = df["Event_Date"].dt.month
     return df
-
-
-def test_get_year_and_month_from_date():
-    df = pd.DataFrame({"Event_Date": ["2022-01-01", "2023-02-02"]})
-    df_expected = pd.DataFrame(
-        {
-            "Event_Date": [pd.Timestamp(2022, 1, 1), pd.Timestamp(2023, 2, 2)],
-            "Event_year": [2022, 2023],
-            "Event_month": [1, 2],
-        }
-    )
-    assert pd.testing.assert_frame_equal(get_year_and_month_from_date(df), df_expected)
 
 
 def filter_by_period(df, column_name: str, start_year: int, end_year: int):
@@ -72,13 +57,14 @@ def get_accident_amount_by_period(df, column_name: str, start_year: int, end_yea
     return amount_of_accidents
 
 
-def cut_unwanted_parts_from_Injury_Severity(df):
-    # def filter_out_non_digits
-    df["Injury_Severity"] = df["Injury_Severity"].str.replace("\W", "", regex=True)
-    df["Injury_Severity"] = df["Injury_Severity"].str.replace("\d+", "", regex=True)
+def remove_symbols_and_digits_from_column(df, column_name):
+    df[column_name] = df[column_name].str.replace(r"\W", "", regex=True)
+    df[column_name] = df[column_name].str.replace(r"\d+", "", regex=True)
+    return df
 
 
-def group_by_injury_severity(df):
+def get_min_max_sum_death_injuries_by_injury_groups(df):
+    """Returns Injury severity groups and min, max and sum death accidents"""
     grouped_by_severity = df.groupby("Injury_Severity")["Total_Fatal_Injuries"].agg(
         ["min", "max", "sum"]
     )
@@ -91,17 +77,21 @@ if __name__ == "__main__":
     df = separate_city_and_state(df)
     df = get_year_and_month_from_date(df)
     print(get_accident_amount_by_period(df, "Event_year", 2020, 2023))
-    print(df.head())
-    # df = cut_unwanted_parts_from_column_values(df)
-    print(group_by_injury_severity(df))
+    df = remove_symbols_and_digits_from_column(df, "Injury_Severity")
+    print(df[["Injury_Severity"]])
+    print(get_min_max_sum_death_injuries_by_injury_groups(df))
 
-    df = read_dataset()
-    (
-        df.pipe(column_name_replacement, ".", "_")
-        .pipe(separate_city_and_state)
-        .pipe(get_year_and_month_from_date)
-    )
 
-    create_visualization = visualize(df)
-    final_report_df = create_report(df)
-    df.to_excel("sdfsdf.xlsx")
+""" Df pipe"""
+# df = read_dataset()
+# (
+#     df.pipe(column_name_replacement, ".", "_")
+#     .pipe(separate_city_and_state)
+#     .pipe(get_year_and_month_from_date)
+# )
+
+
+""" Functions for final data representation"""
+# create_visualization = visualize(df)
+# final_report_df = create_report(df)
+# df.to_excel("sdfsdf.xlsx")
