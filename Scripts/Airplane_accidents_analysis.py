4f1e16a282680d62a4672c4fd3023f419e95dd06
valdasjurk <valdas.jurk@gmail.com>
2023-04-23 23:25:33 +0300

    added results vizualization

--- Scripts/Airplane_accidents_analysis.py
+++ Scripts/Airplane_accidents_analysis.py
@@ -2,35 +2,35 @@
 
 import kaggle
 import pandas as pd
-
 import pandera as pa
-
 from pandera.typing import DataFrame, Series
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 RAW_DATA_DIRECTORY = "data/raw/AviationData.csv"
 
 
 class InputSchema(pa.SchemaModel):
-    Event_Id: Series[str] = pa.Field(coerce=True, nullable=True)
-    Investigation_Type: Series[str] = pa.Field(coerce=True, nullable=True)
-    Accident_Number: Series[str] = pa.Field(coerce=True, nullable=True)
+    Event_Id: Series[str] = pa.Field(nullable=True, coerce=True)
+    Investigation_Type: Series[str] = pa.Field(nullable=True, coerce=True)
+    Accident_Number: Series[str] = pa.Field(nullable=True, coerce=True)
     Event_Date: Series[pd.DatetimeTZDtype] = pa.Field(
         dtype_kwargs={"unit": "ns", "tz": "EST"}, coerce=True, nullable=True
     )
-    Location: Series[str] = pa.Field(coerce=True, nullable=True)
-    Country: Series[str] = pa.Field(coerce=True, nullable=True)
+    Location: Series[str] = pa.Field(nullable=True, coerce=True)
+    Country: Series[str] = pa.Field(nullable=True, coerce=True)
     Latitude: Series[str] = pa.Field(coerce=True, nullable=True)
     Longitude: Series[str] = pa.Field(coerce=True, nullable=True)
-    Airport_Code: Series[str] = pa.Field(coerce=True, nullable=True)
-    Airport_Name: Series[str] = pa.Field(coerce=True, nullable=True)
-    Injury_Severity: Series[str] = pa.Field(coerce=True, nullable=True)
-    Aircraft_damage: Series[str] = pa.Field(coerce=True, nullable=True)
-    Aircraft_Category: Series[str] = pa.Field(coerce=True, nullable=True)
-    Registration_Number: Series[str] = pa.Field(coerce=True, nullable=True)
-    Make: Series[str] = pa.Field(coerce=True, nullable=True)
-    Model: Series[str] = pa.Field(coerce=True, nullable=True)
-    Amateur_Built: Series[str] = pa.Field(coerce=True, nullable=True)
-    Number_of_Engines: Series[str] = pa.Field(coerce=True, nullable=True)
+    Airport_Code: Series[str] = pa.Field(nullable=True, coerce=True)
+    Airport_Name: Series[str] = pa.Field(nullable=True, coerce=True)
+    Injury_Severity: Series[str] = pa.Field(nullable=True, coerce=True)
+    Aircraft_damage: Series[str] = pa.Field(nullable=True, coerce=True)
+    Aircraft_Category: Series[str] = pa.Field(nullable=True, coerce=True)
+    Registration_Number: Series[str] = pa.Field(nullable=True, coerce=True)
+    Make: Series[str] = pa.Field(nullable=True, coerce=True)
+    Model: Series[str] = pa.Field(nullable=True, coerce=True)
+    Amateur_Built: Series[str] = pa.Field(nullable=True, coerce=True)
+    Number_of_Engines: Series[str] = pa.Field(nullable=True, coerce=True)
     Engine_Type: Series[str] = pa.Field(coerce=True, nullable=True)
     FAR_Description: Series[str] = pa.Field(coerce=True, nullable=True)
     Schedule: Series[str] = pa.Field(coerce=True, nullable=True)
@@ -47,10 +47,17 @@ class InputSchema(pa.SchemaModel):
         dtype_kwargs={"unit": "ns", "tz": "EST"}, coerce=True, nullable=True
     )
 
+    # class Config:
+    #     coerce = True
+    #     nullable = True
+
 
 class OutputSchema(InputSchema):
-    City: Series[str] = pa.Field(nullable=True)
-    State: Series[str] = pa.Field(nullable=True)
+    pass
+
+
+# City: Series[str] = pa.Field(nullable=True)
+# State: Series[str] = pa.Field(nullable=True)
 
 
 def download_dataset() -> None:
@@ -74,22 +81,30 @@ def read_dataset():
     return df
 
 
-def column_name_replacement(df, what_to_replace: str, replacement: str) -> pd.DataFrame:
+@pa.check_types
+def column_name_replacement(
+    df, what_to_replace: str, replacement: str
+) -> DataFrame[InputSchema]:
     """Replaces symbols, letters in all column names with a given string"""
     df.columns = df.columns.str.replace(what_to_replace, replacement)
     return df
 
 
-@pa.check_types
-def separate_city_and_state(df: DataFrame[InputSchema]) -> DataFrame[OutputSchema]:
+def separate_city_and_state(df):
     """Separates city and state"""
     df_city_state = df["Location"].str.split(",", n=1, expand=True)
-    return df.assign(City=df_city_state[0], State=df_city_state[1])
+    dfr = df.assign(City=df_city_state[0], State=df_city_state[1])
+    """ plotting events by states count"""
+    sns.countplot(
+        y="State",
+        data=dfr[dfr["Country"] == "United States"],
+    )
+    plt.show()
+    return dfr
 
 
 def create_year_and_month_column_from_date(df) -> pd.DataFrame:
     """Separates year and month from accident date"""
-    # df["Event_Date"] = pd.to_datetime(df["Event_Date"])
     df = df.assign(Event_year=df["Event_Date"].dt.year)
     df = df.assign(Event_month=df["Event_Date"].dt.month)
     return df
@@ -125,7 +140,8 @@ def get_min_max_sum_death_injuries_by_injury_groups(df):
 def timedelta_between_accident_and_publication(df):
     """Calculates difference in days between publication date and event date"""
     timedelta = (df["Publication_Date"] - df["Event_Date"]).dt.days
-    print(timedelta)
+    fig = timedelta.plot.hist(bins=12, legend=True, xlim=(0, 6000))
+    fig.figure.savefig("Timedelta.png")
     return df.assign(Timedelta=timedelta)
 
 
@@ -137,7 +153,7 @@ if __name__ == "__main__":
     df_mod = remove_symbols_and_digits_from_column(df_mod, "Injury_Severity")
     print(get_accident_amount_by_period(df_mod, "Event_year", 2020, 2023))
     print(get_min_max_sum_death_injuries_by_injury_groups(df_mod))
-    timedelta_between_accident_and_publication(df_mod)
+    df_mod = timedelta_between_accident_and_publication(df_mod)
     # print(df.head())
 
     # df = read_dataset()
