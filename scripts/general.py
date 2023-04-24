daf006822523294dfe7fbbcfed286242732db7dc
valdasjurk <valdas.jurk@gmail.com>
2023-04-24 21:12:51 +0300

    added pipelines in main

--- scripts/general.py
+++ scripts/general.py
@@ -70,7 +70,7 @@ def download_dataset() -> None:
     )
 
 
-def read_dataset():
+def read_dataset() -> pd.DataFrame:
     df = pd.read_csv(
         RAW_DATA_DIRECTORY,
         parse_dates=["Event.Date", "Publication.Date"],
@@ -90,7 +90,7 @@ def column_name_replacement(
     return df
 
 
-def separate_city_and_state(df):
+def separate_city_and_state(df: pd.DataFrame) -> pd.DataFrame:
     """Separates city and state"""
     df_city_state = df["Location"].str.split(",", n=1, expand=True)
     dfr = df.assign(City=df_city_state[0], State=df_city_state[1])
@@ -103,18 +103,22 @@ def separate_city_and_state(df):
     return dfr
 
 
-def create_year_and_month_column_from_date(df) -> pd.DataFrame:
+def create_year_and_month_column_from_date(df: pd.DataFrame) -> pd.DataFrame:
     """Separates year and month from accident date"""
     df = df.assign(Event_year=df["Event_Date"].dt.year)
     df = df.assign(Event_month=df["Event_Date"].dt.month)
     return df
 
 
-def filter_by_period(df, column_name: str, start_year: int, end_year: int):
+def filter_by_period(
+    df, column_name: str, start_year: int, end_year: int
+) -> pd.DataFrame:
     return df[(df[column_name] >= start_year) & (df[column_name] <= end_year)]
 
 
-def get_accident_amount_by_period(df, column_name: str, start_year: int, end_year: int):
+def get_accident_amount_by_period(
+    df, column_name: str, start_year: int, end_year: int
+) -> int:
     """Returns accident amount by a given period"""
     df_by_condition = df[
         (df[column_name] >= start_year) & (df[column_name] <= end_year)
@@ -123,13 +127,15 @@ def get_accident_amount_by_period(df, column_name: str, start_year: int, end_yea
     return amount_of_accidents
 
 
-def remove_symbols_and_digits_from_column(df, column_name):
+def remove_symbols_and_digits_from_column(
+    df: pd.DataFrame, column_name: str
+) -> pd.DataFrame:
     df[column_name] = df[column_name].str.replace(r"\W", "", regex=True)
     df[column_name] = df[column_name].str.replace(r"\d+", "", regex=True)
     return df
 
 
-def get_min_max_sum_death_injuries_by_injury_groups(df):
+def get_min_max_sum_death_injuries_by_injury_groups(df: pd.DataFrame) -> pd.DataFrame:
     """Returns Injury severity groups and min, max and sum death accidents"""
     grouped_by_severity = df.groupby("Injury_Severity")["Total_Fatal_Injuries"].agg(
         ["min", "max", "sum"]
@@ -137,37 +143,35 @@ def get_min_max_sum_death_injuries_by_injury_groups(df):
     return grouped_by_severity
 
 
-def timedelta_between_accident_and_publication(df):
+def timedelta_between_accident_and_publication(df: pd.DataFrame) -> pd.DataFrame:
     """Calculates difference in days between publication date and event date"""
     timedelta = (df["Publication_Date"] - df["Event_Date"]).dt.days
+    """ plotting results in histogram """
     fig = timedelta.plot.hist(bins=12, legend=True, xlim=(0, 6000))
     fig.figure.savefig("output/Timedelta.png")
-    return df.assign(Timedelta=timedelta)
+    return df.assign(Time_between_publication_and_event=timedelta)
 
 
 if __name__ == "__main__":
     df = read_dataset()
-    df = column_name_replacement(df, ".", "_")
-    df_mod = separate_city_and_state(df)
-    df_mod = create_year_and_month_column_from_date(df_mod)
-    df_mod = remove_symbols_and_digits_from_column(df_mod, "Injury_Severity")
-    print(get_accident_amount_by_period(df_mod, "Event_year", 2020, 2023))
-    print(get_min_max_sum_death_injuries_by_injury_groups(df_mod))
-    df_mod = timedelta_between_accident_and_publication(df_mod)
-    # print(df.head())
+    df_processed = (
+        df.pipe(column_name_replacement, ".", "_")
+        .pipe(separate_city_and_state)
+        .pipe(create_year_and_month_column_from_date)
+        .pipe(remove_symbols_and_digits_from_column, "Injury_Severity")
+    )
 
-    # df = read_dataset()
-    # (
-    #     df.pipe(column_name_replacement, ".", "_")
-    #     .pipe(separate_city_and_state)
-    #     .pipe(create_year_and_month_column_from_date)
-    #     .pipe(remove_symbols_and_digits_from_column, "Injury_Severity")
-    #     .pipe(get_min_max_sum_death_injuries_by_injury_groups)
-    # .pipe(get_accident_amount_by_period("Event_year", 2020, 2023))
-    # )
+    death_injuries_statistics = get_min_max_sum_death_injuries_by_injury_groups(
+        df_processed
+    )
+    accidents_by_period = get_accident_amount_by_period(
+        df_processed, "Event_year", 2020, 2023
+    )
+    print(death_injuries_statistics)
+    print(accidents_by_period)
 
 
-""" Functions for final data representation"""
+""" Future Functions for final data representation"""
 # create_visualization = visualize(df)
 # final_report_df = create_report(df)
 # df.to_excel("sdfsdf.xlsx")
