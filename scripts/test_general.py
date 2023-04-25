96583d9da57062e89a2aa7259ad17b41e92c0d65
valdasjurk <valdas.jurk@gmail.com>
2023-04-25 20:22:05 +0300

    added tests to majority function fixed #6

--- scripts/test_general.py
+++ scripts/test_general.py
@@ -1,4 +1,10 @@
-from general import create_year_and_month_column_from_date
+from general import (
+    create_year_and_month_column_from_date,
+    timedelta_between_accident_and_publication,
+    remove_symbols_and_digits_from_column,
+    separate_city_and_state,
+    column_name_replacement,
+)
 import pandas as pd
 
 
@@ -15,4 +21,70 @@ def test_get_year_and_month_from_date():
     )
     pd.testing.assert_frame_equal(
         create_year_and_month_column_from_date(df), df_expected
+    )
+
+
+def test_timedelta_in_days():
+    df = pd.DataFrame(
+        {
+            "Event_Date": [pd.Timestamp(2022, 1, 1), pd.Timestamp(2023, 2, 2)],
+            "Publication_Date": [pd.Timestamp(2022, 1, 2), pd.Timestamp(2023, 2, 5)],
+        }
+    )
+    df_expected = pd.DataFrame(
+        {
+            "Event_Date": [pd.Timestamp(2022, 1, 1), pd.Timestamp(2023, 2, 2)],
+            "Publication_Date": [pd.Timestamp(2022, 1, 2), pd.Timestamp(2023, 2, 5)],
+            "Time_between_publication_and_event": [1, 3],
+        }
+    )
+    pd.testing.assert_frame_equal(
+        timedelta_between_accident_and_publication(df), df_expected
+    )
+
+
+def test_remove_symbols_and_digits_from_column_values():
+    df = pd.DataFrame(
+        {
+            "Injury": ["Fatal2", "Serious(1)"],
+        }
+    )
+    df_expected = pd.DataFrame(
+        {
+            "Injury": ["Fatal", "Serious"],
+        }
+    )
+    pd.testing.assert_frame_equal(
+        remove_symbols_and_digits_from_column(df, "Injury"), df_expected
+    )
+
+
+def test_separate_city_and_state():
+    df = pd.DataFrame(
+        {"Location": ["Seaside Heights,NJ"], "Country": ["United States"]}
+    )
+    df_expected = pd.DataFrame(
+        {
+            "Location": ["Seaside Heights,NJ"],
+            "Country": ["United States"],
+            "City": ["Seaside Heights"],
+            "State": ["NJ"],
+        }
+    )
+    pd.testing.assert_frame_equal(separate_city_and_state(df), df_expected)
+
+
+def test_column_name_replacement():
+    df = pd.DataFrame(
+        {
+            "Injury-total": [1, 2],
+        }
+    )
+    df_expected = pd.DataFrame(
+        {
+            "Injury_total": [1, 2],
+        }
+    )
+    pd.testing.assert_frame_equal(
+        column_name_replacement.__wrapped__(df, "-", "_"), df_expected
     )
