56a6989f36512ffd0fbce021490c2f0603e0eea1
valdasjurk <valdas.jurk@gmail.com>
2023-04-25 21:16:28 +0300

    Initial commit

--- scripts/run.py
+++ scripts/run.py
@@ -5,6 +5,7 @@ from general import (
     get_min_max_sum_death_injuries_by_injury_groups,
     get_accident_amount_by_period,
 )
+import argparse
 
 
 def download_data():
@@ -23,3 +24,32 @@ def get_results():
     accidents_by_period = get_accident_amount_by_period(df, "Event_year", 2020, 2023)
     print("Death injuries statistics: ", death_injuries_statistics)
     print("Accidents by period: ", accidents_by_period)
+
+
+if __name__ == "__main__":
+    # Get our arguments from the user
+    parser = argparse.ArgumentParser(description=__doc__)
+
+    parser.add_argument(
+        "--download_data", help="Download kaggle dataset", action="store_true"
+    )
+    parser.add_argument(
+        "--preprocesse",
+        help="Function returns preprocessed dataframe",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--get_results",
+        help="Get statistics and amount of accidents",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    if args.download_data:
+        download_data()
+
+    if args.preprocesse:
+        preprocesse()
+
+    if args.get_results:
+        get_results()
