34fdd6e8032dfbcce8e9df44b9c53d1271411a63
valdasjurk <valdas.jurk@gmail.com>
2023-05-03 14:04:51 +0300

    initial commit airport api example

---
+++ scripts/airport_api.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+""" Prints airport full name by its iata code"""
+
+params = {
+    "api_key": "ef006315-1a52-4296-8ba9-124b6fb58f67",
+    "iata_code": ["KUN, JAX, EWR, VNO"],
+}
+method = "ping"
+api_base = "https://airlabs.co/api/v9/airports?"
+api_result = requests.get(api_base + method, params)
+data = api_result.text
+data_dict = json.loads(data)
+
+for key in data_dict["response"]:
+    print(key["name"])
