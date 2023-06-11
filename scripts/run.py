a7bb9e8b3a634177f6504962ca4df3cf21c48e63
valdasjurk <valdas.jurk@gmail.com>
2023-06-11 22:31:42 +0300

    separate get_results function and fix #22

--- scripts/run.py
+++ scripts/run.py
@@ -1,25 +1,28 @@
 from general import (
     load_dataset,
     preprocese_dataset,
-    get_min_max_sum_death_injuries_by_injury_groups,
     get_accident_amount_by_period,
     save_to_csv,
+    accident_statistics_by_airplane_make_engine_flight_purpose,
 )
 import argparse
+import config
+import pandas as pd
 
 
-def preprocesse():
+def prepare_and_save_data():
     df = load_dataset()
     df_processed = preprocese_dataset(df)
     save_to_csv(df_processed)
-    return df_processed
 
 
-def get_results(start, end):
-    df = preprocesse()
-    get_min_max_sum_death_injuries_by_injury_groups(df)
+def load_preprocessed_data(path=config.INTERIM_DIRECTORY):
+    return pd.read_csv(path, low_memory=False)
+
+
+def get_accidents_by_period(df, start, end):
     accidents_by_period = get_accident_amount_by_period(df, "Event_year", start, end)
-    print("Accidents by period: ", accidents_by_period)
+    return accidents_by_period
 
 
 if __name__ == "__main__":
@@ -30,24 +33,43 @@ if __name__ == "__main__":
         "--load_dataset", help="Download kaggle dataset", action="store_true"
     )
     parser.add_argument(
-        "--preprocesse",
+        "--prepare_and_save_data",
         help="Function returns preprocessed dataframe",
         action="store_true",
     )
     parser.add_argument(
-        "--get_results",
-        help="Get statistics and amount of accidents",
+        "--get_accidents_by_period",
+        help="Get statistics and amount of accidents. Requires prepared data. See --load_and_save_data",
         action="store_true",
     )
     parser.add_argument("--start", type=int)
     parser.add_argument("--end", type=int)
+
+    parser.add_argument(
+        "--get_statistics_airplane_make_engine_flight_purpose",
+        help="Function returns dataframe with airplane make, engine type and flight purpose that gets into accidents most frequently",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     if args.load_dataset:
         load_dataset()
 
-    if args.preprocesse:
-        preprocesse()
+    if args.prepare_and_save_data:
+        prepare_and_save_data()
 
-    if args.get_results:
-        get_results(args.start, args.end)
+    if args.get_accidents_by_period:
+        try:
+            df = load_preprocessed_data()
+            results = get_accidents_by_period(df, args.start, args.end)
+            print(results)
+        except FileNotFoundError:
+            print("No prepared data found. Did you run --prepare_and_save_data ?")
+
+    if args.get_statistics_airplane_make_engine_flight_purpose:
+        try:
+            df = load_preprocessed_data()
+            results = accident_statistics_by_airplane_make_engine_flight_purpose(df)
+            print(results)
+        except FileNotFoundError:
+            print("No prepared data found. Did you run --prepare_and_save_data ?")
